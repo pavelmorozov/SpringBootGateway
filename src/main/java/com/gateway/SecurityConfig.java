20a9dc88d20235ce97d68bce87c377d865f887e9
pm <pm@pm-mongo>
2018-09-26 14:13:14 +0300

    Gateway works without security now.

--- src/main/java/com/gateway/SecurityConfig.java
+++ src/main/java/com/gateway/SecurityConfig.java
@@ -1,48 +1,48 @@
-package com.gateway;
-
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.faircloud.config.JwtConfig;
-
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Autowired
-	private JwtConfig jwtConfig;
-
-	@Bean
-	public JwtConfig jwtConfig() {
-		return new JwtConfig();
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-
-		http.csrf().disable()
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-				// handle an authorized attempts
-			.exceptionHandling()
-			.authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)).and()
-			// Add a filter to validate the tokens with every request
-			.addFilterAfter(new JwtAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class)
-			// authorization requests config
-			.authorizeRequests()
-			// allow all who are accessing "auth" service
-			.antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()
-			// must be an admin if trying to access admin area (authentication is also
-			// required here)
-			//.antMatchers("/gallery" + "/admin/**").hasRole("ADMIN")
-			.antMatchers("/banquet/**").hasRole("USER")
-			// Any other request must be authenticated
-			.anyRequest().authenticated();
-	}
-}
+//package com.gateway;
+//
+//import javax.servlet.http.HttpServletResponse;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//import com.faircloud.config.JwtConfig;
+//
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//	@Autowired
+//	private JwtConfig jwtConfig;
+//
+//	@Bean
+//	public JwtConfig jwtConfig() {
+//		return new JwtConfig();
+//	}
+//
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//
+//		http.csrf().disable()
+//			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+//				// handle an authorized attempts
+//			.exceptionHandling()
+//			.authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)).and()
+//			// Add a filter to validate the tokens with every request
+//			.addFilterAfter(new JwtAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class)
+//			// authorization requests config
+//			.authorizeRequests()
+//			// allow all who are accessing "auth" service
+//			.antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()
+//			// must be an admin if trying to access admin area (authentication is also
+//			// required here)
+//			//.antMatchers("/gallery" + "/admin/**").hasRole("ADMIN")
+//			.antMatchers("/banquet/**").hasRole("USER")
+//			// Any other request must be authenticated
+//			.anyRequest().authenticated();
+//	}
+//}
