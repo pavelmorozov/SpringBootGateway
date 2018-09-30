5b04ef2e1e8bd84e6f7730b2a0bd0aee8654adbd
pm <pm@faircloud>
2018-09-30 23:18:38 +0300

    Zuul gateway works

--- src/main/java/com/gateway/GatewayApplication.java
+++ src/main/java/com/gateway/GatewayApplication.java
@@ -3,11 +3,44 @@ package com.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+//@EnableEurekaClient 		// It acts as a eureka client
+@EnableZuulProxy		// Enable Zuul
 public class GatewayApplication {
 
+//	@Autowired
+//	AuthenticationManager authenticationManager;
+//	
+//	@Bean
+//	SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) throws Exception {
+//		return http.httpBasic().and()
+//				.csrf().disable()
+//				.formLogin().disable()
+//				.authenticationManager(authenticationManager)
+//				.authorizeExchange()
+//				.pathMatchers("/**").permitAll()
+//				//.pathMatchers("/anything/**").authenticated()
+//				//.antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()
+//				.anyExchange().permitAll()
+//				.and().build();
+//		
+		
+		
+		
+		//return http
+				//.authenticationManager(authenticationManager)
+				//.securityContextRepository(securityContextRepository)
+				//.authorizeExchange()
+				//.pathMatchers(HttpMethod.OPTIONS).permitAll()
+				//.pathMatchers("/auth").permitAll()
+
+
+		
+	//}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
---
+++ src/main/java/com/gateway/security/reactive/Role.java
@@ -0,0 +1,9 @@
+//package com.gateway.security.reactive;
+//
+///**
+// *
+// * @author ard333
+// */
+//public enum Role {
+//	ROLE_USER, ROLE_ADMIN
+//}
